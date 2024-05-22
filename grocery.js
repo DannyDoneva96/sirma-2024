e9257ad5d0dfe6a2d8f35e4bc344e3ecd7f51cf8
DannyDoneva96 <danielasinmas1@gmail.com>
2024-05-22 14:33:09 +0300

    Create grocery.js

---
+++ grocery.js
@@ -0,0 +1,36 @@
+ function grocery(product,city,quantity){
+
+    let prices= {
+
+       Sofia: {
+        tea:0.50,
+        water:0.80,
+        juice:1.20,
+        sweets:1.45,
+        chips:1.60,
+
+       },Varna: {
+        tea:0.45,
+        water:0.70,
+        juice:1.15,
+        sweets:1.35,
+        chips:1.55,
+
+       },Plovdiv: {
+        tea:0.40,
+        water:0.70,
+        juice:1.15,
+        sweets:1.30,
+        chips:1.50,
+
+       },
+
+    }
+    let result = prices[city][product] * quantity
+
+    return result.toFixed(2)
+}
+
+console.log(grocery('tea','Varna',2))
+console.log(grocery('juice','Sofia',6))
+console.log(grocery('sweets','Plovdiv',1))
